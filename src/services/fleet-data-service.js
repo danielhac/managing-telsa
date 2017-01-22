af24ee85439c317ea218e93b4ed1527f801b3603
Dan <danhboi23@hotmail.com>
2017-01-21 17:17:24 -0800

    Added error handling

--- src/services/fleet-data-service.js
+++ src/services/fleet-data-service.js
@@ -1,10 +1,12 @@
 import {Car} from '../classes/car.js';
 import {Drone} from '../classes/drone.js';
+import {DataError} from './data-error.js';
 
 export class FleetDataService {
     constructor() {
         this.cars = [];
         this.drones = [];
+        this.errors = [];
     }
 
     loadData(fleet) {
@@ -17,15 +19,24 @@ export class FleetDataService {
                 case 'drone':
                     this.drones.push(data);
                     break;
+                default:
+                    let e = new DataError('Invalid vehicle type', data);
+                    this.errors.push(e);
+                    break;
             }
         }
     }
 
     loadCar(car) {
-        let c = new Car(car.license, car.model, car.latLong);
-        c.miles = car.miles;
-        c.make = car.make;
-        return c;
+        try {
+            let c = new Car(car.license, car.model, car.latLong);
+            c.miles = car.miles;
+            c.make = car.make;
+            return c;
+        } catch (e) {
+            this.errors.push(new DataError('Error loading car', car));
+        }
+        return null;
     }
 
 }
